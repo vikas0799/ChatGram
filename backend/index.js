773c8ac5d3e1844c56d2be78d4524fee2953ec27
Vikas Patel <vikaspatel07071999@gmail.com>
2024-09-02 23:59:12 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/auth");
+const messageRoutes = require("./routes/messages");
+const app = express();
+const socket = require("socket.io");
+require("dotenv").config();
+
+app.use(cors({
+  origin: process.env.ORIGIN || "http://localhost:3000", // Use ORIGIN from .env
+  credentials: true
+}));
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connection Successful");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+app.get("/ping", (_req, res) => {
+  return res.json({ msg: "Ping Successful" });
+});
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+const port = process.env.PORT || 5000; // Default to port 5000 if PORT is not specified
+const server = app.listen(port, () =>
+  console.log(`Server started on port ${port}`)
+);
+
+const io = socket(server, {
+  cors: {
+    origin: process.env.ORIGIN || "http://localhost:3000", // Use ORIGIN from .env
+    credentials: true,
+  },
+});
+
+global.onlineUsers = new Map();
+io.on("connection", (socket) => {
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
+
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-receive", data.msg);
+    }
+  });
+});
